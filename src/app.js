646023ae0e686adffdcfc71b7951a8740c09b545
Alex Ofoedu <alexander@junglecreations.com>
2017-10-26 09:18:34 +0100

    woo

--- src/app.js
+++ src/app.js
@@ -54,17 +54,16 @@ app.post("/add", function (req, res) {
 	});
 });
 
+app.post("/upd", function (req, res) {
+
+	var attrs = { name: req.body.name, developer: req.body.developer, genre: req.body.genre, year: req.body.year };
+	Game.findByIdAndUpdate(req.body._id, attrs, function (err) {
+		if (err) return console.log(err);
+		res.redirect("/");
+	});
+});
+
 app.post("/del", function (req, res) {
-
-	// let {theid} = req.body;
-	// db.collection("games").findByIdAndRemove(req.body._id, (err) => {
-
-	// 	if (err) return console.log(err);
-	// 	res.redirect("/");
-
-	// });
-
-	// let theGame = new Game();
 
 	Game.findByIdAndRemove(req.body._id, function (err) {
 		if (err) return console.log(err);
@@ -72,143 +71,4 @@ app.post("/del", function (req, res) {
 	});
 });
 
-// function renderHtml(res, data) {
-// 	res.render("html/index.html", data);
-// }
-
-
-// function gameCreate(name, developer, genre, year, cb)
-// {
-// 	let gameAttr = {name: name, developer: developer, genre: genre, year: year};
-
-// 	let game = new Game(gameAttr);
-
-// 	game.save(() => {
-
-// 		if (err)
-// 		{
-// 			cb(err, null);
-// 			return;
-// 		}
-
-// 		console.log("New Game! " + "name: " + name + "genre" + genre);
-// 		genre.push(genre);
-// 		cb(null, genre);
-// 	});
-// }
-
 app.listen(4560);
-
-// let dbConn = mongoose.connect("mongodb://localhost/NodeTestDB");
-
-// var dex = new User({
-// 	name: "Ayatollah",
-// 	username: "ayatollah",
-// 	password: "jihadwarrior"
-// });
-
-// dex.dudify((err, name) => {
-// 	if (err) throw err;
-
-// 	console.log("Your dude name is: " + name);
-// });
-
-// dex.save((err)=>{
-// 	if (err) throw err;
-
-// 	console.log("User saved successfully");
-// });
-
-// dbConn.disconnect();
-
-
-// const model = require("./schema.js");
-
-// const mongoDb = "mongodb://localhost/NodeTestDB";
-
-// mongoose.connect(mongoDb, {
-// 	useMongoClient: true
-// })
-
-// const db = mongoose.connection;
-
-// db.on("error", console.error.bind(console, "Connection error"));
-
-// const bodyParser = require("body-parser");
-
-
-// const MongoClient = mongoDb.MongoClient;
-// const ObjectId = mongoDb.ObjectId;
-
-
-// app.set("view engine", "ejs");
-
-
-// mongoose.connect("mongodb://localhost/test");
-
-// let db = mongoose.connection;
-// db.on("error", console.error.bind(console, "connection error:"));
-// db.once("open", function() {
-// });
-
-// app.get("/", (req, res) => {
-// 	res.sendFile(__dirname + "/index.html")
-// });
-
-// app.listen(4560);
-
-
-/////////////////////
-// MongoClient.connect("mongodb://localhost:27017/NodeTestDB", (err, database) => {
-// 	if (err) return console.log(err);
-// 	db = database;
-// 	app.listen(58531, () => {
-// 		console.log("connection established with NoSQL database");
-// 	});
-// });
-
-// app.use(bodyParser.urlencoded({ extended: true }));
-
-// app.get("/", (req, res) => {
-
-// 	if (db.collection("games"))
-// 	{
-// 		db.collection("games").find().toArray((err, result) => {
-// 			if (err) return console.log(err);
-// 			renderHtml(res, {games: result});     
-// 		});
-// 	}
-// 	else{
-// 		renderHtml(res);
-// 	}
-// });
-
-// app.post("/del", (req, res) => {
-// 	const {_id} = req.body;
-// 	console.log(req.body);
-// 	db.collection("games").deleteOne({_id: ObjectId(_id)}, (err) => {
-// 		if (err) return console.log(err);
-// 		console.log(req.body);        
-// 		res.redirect("/");
-// 		// console.log('entry deleted')
-
-
-// 	});
-// });
-
-// app.post("/add", (req, res) => {
-// 	db.collection("games").save(req.body, (err, result) => {
-// 		if (err) return console.log(err);
-
-// 		console.log("saved to database");
-// 		res.redirect("/");
-// 	});
-// });
-// function renderHtml (res, data) {
-// 	res.render("../index.html", data);
-// }
-// app.get("/", (req, res) => {
-// 	renderHtml(res);
-// });
-
-// app.listen(4560);
